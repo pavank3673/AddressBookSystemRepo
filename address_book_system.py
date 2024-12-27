8d9b16f9de761d5ef63d239c25e15bb23434460a
pavank3673 <pavank3673@outlook.com>
2024-12-27 17:22:29 +0530

    [Pavan Kumar]Added:add new contact to address book

--- address_book_system.py
+++ address_book_system.py
@@ -17,4 +17,18 @@ class ContactPerson:
 class AddressBook:
     def __init__(self, contact_person):
         self.contact_person = contact_person
-        
+
+print("--- Enter contact details ---")
+first_name = input("Enter first name : ")
+last_name = input("Enter last name : ")
+address = input("Enter address : ")
+city = input("Enter city : ")
+state = input("Enter state : ")
+zip = input("Enter zip : ")
+phone_number = input("Enter phone number : ")
+email = input("Enter email : ")
+
+contact_one = ContactPerson(first_name, last_name, address, city, state, zip, phone_number, email)
+address_book_one = AddressBook(contact_one)
+
+print(f"{address_book_one.contact_person} added to address book successfully")
