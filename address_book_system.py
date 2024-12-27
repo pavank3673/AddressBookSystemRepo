4f1122d8a8e71c93095e349cff7852dbaa3294e1
pavank3673 <pavank3673@outlook.com>
2024-12-27 18:41:25 +0530

    [Pavan Kumar]Added:edit existing contact using name in address book

--- address_book_system.py
+++ address_book_system.py
@@ -32,6 +32,22 @@ class AddressBook:
         new_contact = ContactPerson(first_name, last_name, address, city, state, zip, phone_number, email)
         self.contact_person = new_contact
 
+    def edit_contact(self):
+        first_name = input("Enter contact person name to edit : ")
+        if self.contact_person.first_name != first_name:
+            print(f"Contact person {first_name} does not exists in address book")
+        else:
+            print("--- Enter contact details ---")
+            self.contact_person.address = input("Enter address : ")
+            self.contact_person.city = input("Enter city : ")
+            self.contact_person.state = input("Enter state : ")
+            self.contact_person.zip = input("Enter zip : ")
+            self.contact_person.phone_number = input("Enter phone number : ")
+            self.contact_person.email = input("Enter email : ")
+
 address_book_one = AddressBook()
 address_book_one.add_contact()
 print(f"{address_book_one.contact_person} added to address book successfully")
+
+address_book_one.edit_contact()
+print(f"{address_book_one.contact_person} edited in address book successfully")
