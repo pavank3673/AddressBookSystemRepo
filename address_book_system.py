6c2290db03cb9659ea04c57a941a1653e70cfe00
pavank3673 <pavank3673@outlook.com>
2024-12-27 16:54:09 +0530

    [Pavan Kumar]Added:create contact in address book

--- address_book_system.py
+++ address_book_system.py
@@ -1,1 +1,20 @@
 print("Welcome to Address Book Program")
+
+class ContactPerson:
+    def __init__(self, first_name, last_name, address, city, state, zip, phone_number, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.phone_number = phone_number
+        self.email = email
+
+    def __str__(self):
+        return f"Contact of {self.first_name} {self.last_name} with phone number : {self.phone_number}"
+    
+class AddressBook:
+    def __init__(self, contact_person):
+        self.contact_person = contact_person
+        
